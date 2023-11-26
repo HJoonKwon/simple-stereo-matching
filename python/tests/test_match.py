bd534163c9e635828d9a007cc15507568da2f824
HJoonKwon <dsazz801@gmail.com>
2023-11-26 17:40:20 +0900

    update: unit test for match

--- python/tests/test_match.py
+++ python/tests/test_match.py
@@ -2,14 +2,14 @@ import torch
 from simple_stereo_matching.match.matcher import Matcher
 
 def test_matcher():
-    matcher = Matcher()
-    left_img = torch.randn((1, 32, 32))
-    right_img = torch.randn((1, 32, 32))
-    max_disparity = 10
+    left_img = Matcher.read_img_as_tensor('../data/Adirondack_left.png')
+    right_img = Matcher.read_img_as_tensor('../data/Adirondack_right.png')
+    assert len(left_img.shape) == 2 and len(right_img.shape) == 2
+    max_disparity = left_img.shape[1]
     window_size = 3 
     metric_name = 'ssd'
-    disparity_map = matcher.compute_disparity(left_img, right_img, max_disparity, window_size, metric_name)
+    disparity_map = Matcher.compute_disparity(left_img, right_img, max_disparity, window_size, metric_name)
     assert (
-        disparity_map.shape[0] == left_img.shape[1]
-        and disparity_map.shape[1] == left_img.shape[2]
+        disparity_map.shape[0] == left_img.shape[0]
+        and disparity_map.shape[1] == left_img.shape[1]
     )
